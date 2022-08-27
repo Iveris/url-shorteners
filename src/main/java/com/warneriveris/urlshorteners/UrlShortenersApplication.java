e9e64a73bd1fcefe83e22d40ea0df25605ea5d20
SuperWarnerMan <WarnerIveris@gmail.com>
2022-08-26 22:30:48 -0700

    created

---
+++ src/main/java/com/warneriveris/urlshorteners/UrlShortenersApplication.java
@@ -0,0 +1,13 @@
+package com.warneriveris.urlshorteners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortenersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortenersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/warneriveris/urlshorteners/UrlShortenersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warneriveris.urlshorteners;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
